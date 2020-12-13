358d0dfeb80b4c8568f047c15e1cceeac42c7f5a
Yelyzaveta <elizaveta.kzm@gmail.com>
2020-12-13 13:04:44 +0100

    Fetching YouTube API sample

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+const baseUrl = "https://www.googleapis.com/youtube/v3";
+const apiKey = "AIzaSyCvCEh7UqPqWwXuXdnwxuIWlTVB0wJHW48";
+
+const INITIAL_RESULTS_MAX_NUMBER = 5;
+
+const videosFromServerFormat = (data) =>
+  data.items.map((item) => ({
+    image: item.snippet.thumbnails.default.url,
+    title: item.snippet.title,
+    videoId: item.id.videoId,
+  }));
+
+const fetchVideos = ({ limit, query }) =>
+  fetch(
+    `${baseUrl}/search?part=snippet&maxResults=${limit}&q=${query}&type=video&key=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then(videosFromServerFormat);
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [resultsMaxNumber, setResultsMaxNumber] = useState(
+    INITIAL_RESULTS_MAX_NUMBER
+  );
+
+  const onVideoSearch = () => {
+    fetchVideos({ limit: resultsMaxNumber, query: inputValue }).then(setVideos);
+  };
+
+  const onYouTubeVideoRedirect = (videoId) => {
+    window.open(`https://www.youtube.com/watch?v=${videoId}`, "_blank");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="content">
+      <div className="searchArea">
+        <input onChange={(e) => setInputValue(e.target.value)} />
+
+        <button onClick={onVideoSearch}>Show results</button>
+
+        <label htmlFor="resultsMaxNumber">Results max number:</label>
+        <select
+          name="resultsMaxNumber"
+          id="resultsMaxNumber"
+          onChange={(e) => setResultsMaxNumber(Number(e.target.value))}
         >
-          Learn React
-        </a>
-      </header>
+          <option value="5">5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+        </select>
+      </div>
+      <div className="videosWrapper">
+        {videos.map((video) => (
+          <div
+            key={video.videoId}
+            className="video"
+            onClick={() => onYouTubeVideoRedirect(video.videoId)}
+          >
+            <img key={video.image} alt="" src={video.image} />
+            <div className="videoTitle">{video.title}</div>
+          </div>
+        ))}
+      </div>
+
+      <pre>{JSON.stringify(videos, null, 2)}</pre>
     </div>
   );
-}
+};
 
 export default App;
