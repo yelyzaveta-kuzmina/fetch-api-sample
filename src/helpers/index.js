1427afde2399eab7dc6a96f4b9d797b42b9d1b6b
Yelyzaveta <elizaveta.kzm@gmail.com>
2020-12-25 16:04:55 +0100

    sending to server a serched word + svg image with generic color

---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+export const customSvg = (color, svgBody) => {
+  const stringifiedSvg = JSON.stringify(svgBody);
+  const updatedSvg = stringifiedSvg.replace("fill:#000", `fill:${color}`);
+  return JSON.parse(updatedSvg);
+};
