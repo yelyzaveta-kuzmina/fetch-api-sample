c372a1b0a2afef6c7b0381534c547b1e7bfb89b9
Yelyzaveta <elizaveta.kzm@gmail.com>
2020-12-25 14:22:47 +0100

    worked on UI for WikiApiHandler + fetching HTML from a second server

--- src/modules/Wiki/index.js
+++ src/modules/Wiki/index.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 const WikiApiHandler = () => {
-  return <div className="wikiHandlerContent">123</div>;
+  const [inputValue, setInputValue] = useState(null);
+  const [data, setData] = useState(null);
+
+  const fetchHTMLContent = async () => {
+    const fetchedData = await fetch(`http://localhost:8080`).then((res) =>
+      res.text()
+    );
+
+    setData(fetchedData);
+
+    if (fetchedData.includes(inputValue)) {
+      alert("Word found and added to the local videosDB!");
+    }
+  };
+
+  return (
+    <div className="wikiHandlerContent">
+      <div className="searchArea">
+        <input
+          placeholder="Type a word to search for"
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button disabled={!inputValue} onClick={() => fetchHTMLContent()}>
+          Search
+        </button>
+        <pre>
+          {data ? "Fetched HTML:" : null}
+          <br />
+          <br />
+          <br />
+          {data}
+        </pre>
+      </div>
+    </div>
+  );
 };
 
 export default WikiApiHandler;
