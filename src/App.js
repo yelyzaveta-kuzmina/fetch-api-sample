ccab84215a324e5ccbe4b3cf89d14556f47290c7
Yelyzaveta <elizaveta.kzm@gmail.com>
2020-12-30 20:25:25 +0100

    react use for keeping local storage data

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useCallback } from "react";
+import { useLocalStorage } from "react-use";
 import Switch from "./components/switcher";
 import YouTubeApiHandler from "./modules/YouTube";
 import WikiApiHandler from "./modules/Wiki";
 import "./index.css";
 
 const App = () => {
-  const [isSwitcherChecked, setSwitcherChecked] = useState();
+  const [isSwitcherChecked, setIsSwitcherChecked] = useLocalStorage(
+    "isSwitcherChecked",
+    false
+  );
 
-  useEffect(() => {
-    const isSwitcherCheckedInStorage = localStorage.getItem(
-      "isSwitcherChecked"
-    );
-
-    if (isSwitcherCheckedInStorage === null) {
-      setSwitcherChecked(false);
-      localStorage.setItem("isSwitcherChecked", 0);
-      return;
-    }
-    setSwitcherChecked(Number(isSwitcherCheckedInStorage));
-  }, []);
-
-  const onSwitcherToggle = useCallback(() => {
-    if (isSwitcherChecked) {
-      setSwitcherChecked(false);
-      localStorage.setItem("isSwitcherChecked", 0);
-      return;
-    }
-    setSwitcherChecked(true);
-    localStorage.setItem("isSwitcherChecked", 1);
-  }, [isSwitcherChecked]);
+  const onSwitcherToggle = useCallback(
+    () => setIsSwitcherChecked(!isSwitcherChecked),
+    [isSwitcherChecked, setIsSwitcherChecked]
+  );
 
   return (
     <div className="appWrapper">
       <div className="controller">
         <span className="moduleName">YouTube</span>
         <Switch
-          onChange={() => onSwitcherToggle()}
+          onChange={onSwitcherToggle}
           checked={Boolean(isSwitcherChecked)}
         />
         <span className="moduleName">Wiki</span>
